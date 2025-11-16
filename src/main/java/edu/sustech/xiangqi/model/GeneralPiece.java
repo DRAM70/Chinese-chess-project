d71f10107736f791e1a7bcbe09531aaa5bcd065f
borrow-moonlight <jiang744939895@qq.com>
2025-11-17 00:36:57 +0800

    general

--- src/main/java/edu/sustech/xiangqi/model/GeneralPiece.java
+++ src/main/java/edu/sustech/xiangqi/model/GeneralPiece.java
@@ -12,6 +12,25 @@ public class GeneralPiece extends AbstractPiece {
     @Override
     public boolean canMoveTo(int targetRow, int targetCol, ChessBoardModel model) {
         // TODO: 实现将/帅的移动规则
-        return true;
+        int currentRow = getRow();
+        int currentCol = getCol();
+        if (currentRow == targetRow && currentCol == targetCol) {
+            return false;
+        }
+
+        //将和帅的移动规则
+        //只能横向和纵向移动一格且只能在九宫内移动
+        int rowDiff = Math.abs(targetRow - currentRow);
+        int colDiff = Math.abs(targetCol - currentCol);
+        if(((rowDiff==1&&colDiff==0)||(rowDiff==0&&colDiff==1))){
+            if (isRed()){
+                return targetRow <= 9 && targetRow >= 7 && targetCol >= 3 && targetCol <= 5;
+            }
+            else {
+                return targetCol >= 3 && targetCol <= 5 && targetRow >= 0 && targetRow <= 2;
+            }
+        }
+
+        return false;
     }
 }
